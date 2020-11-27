5fbff4feb4e9bf01f4a65f82c1022e91e5b9b7d6
dayuhidayatullah <dayuhidayat123@gmail.com>
2020-11-27 13:42:06 +0700

    add-jquery-login-register-fetch-music

--- assets/helpers.js
+++ assets/helpers.js
@@ -13,3 +13,103 @@ function onSignIn(googleUser) {
         console.log(xhr)
     })
 }
+function showMainPage(){
+    $("#login-page").hide()
+    $("#register-page").hide()
+    $("#list-music").show()
+    $("#btn-logout").show()
+    
+}
+function showLoginPage(){
+    $("#login-page").show()
+    $("#register-page").hide()
+    $("#list-music").hide()
+    $("#btn-logout").hide()
+}
+function logout(){
+    localStorage.clear()
+    showLoginPage()
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut()
+    .then(function () {
+        console.log('User signed out.');
+    });
+}
+
+
+function registerPage(){
+    $("#login-page").hide()
+    $("#register-page").show()
+    $("#list-music").hide()
+    $("#btn-logout").hide()
+}
+function register(){
+    const email = $("#email-regis").val
+    const password = $("#password-regis").val()
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/register",
+            data: {
+                email,
+                password
+            }
+        })
+        .done(response => {
+            console.log(response)
+            showLoginPage()
+        })
+        .fail((xhr, textStatus) => {
+            console.log(textStatus)
+        })
+}
+function login(){
+    const email = $("#email-login").val()
+        const password = $("#password-login").val()
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/login",
+            data: {
+                email,
+                password
+            }
+        })
+        .done(response => {
+            console.log(response)
+            localStorage.setItem('access_token', response.access_token)
+            showMainPage()
+        })
+        .fail((xhr, textStatus) => {
+            console.log(textStatus)
+        })
+        .always(_ => {
+            $("#login-form").trigger("reset")
+        })
+}
+function fetchMusic(){
+    // $("#list-music").empty()
+    $.ajax({
+        method: "GET",
+        url:  `${server}/todos`,
+        headers: {
+            access_token: localStorage.getItem("access_token")
+        }
+    })
+    .done(response => {
+        response.forEach(element => {
+            // console.log(element.id)  
+            $("#todo-list").append(` 
+            <div class="card bg-ligth mb=10 pd=10" id="music-list">
+                <div class="card-deck m">
+                    <div class="card-body text-center">
+                        <p class="card-text">${element.title}</p>
+                        <p class="card-text">${element.description}</p>
+                        <p class="card-text text-muted"> ${element.due_date.substr(0, 10)}</p>
+                    </div>
+                </div>
+            </div>`)
+        });
+    })
+    .fail(xhr => {
+        console.log(xhr)
+    })
+}
